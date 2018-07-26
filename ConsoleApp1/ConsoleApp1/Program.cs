76a7906b1175a5d91a188e3347d6b3d0b075fd50
Helen Brennan <9981703@student.toiohomai.ac.nz>
2018-07-26 14:13:36 +1200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.ReadLine();
+        
+            Console.WriteLine("Good Bye");
+            Console.ReadKey();
+
+        }
+    }
+}
